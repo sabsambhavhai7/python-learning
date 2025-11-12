0265a6922caa85003b9de9be9af3d3eb933d3067
sabsambhavhai7 <shuklasambhav48@gmail.com>
2025-11-12 14:19:21 +0545

    Initial commit with pyjokes script

---
+++ chap_modules_pip.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+# print("printing jokes...") 
+
+# this printts a random joke
+# this printts a random joke
+# this printts a random joke
+# this printts a random joke
+
+joke= pyjokes.get_jokes()
+print(joke)
